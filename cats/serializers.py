9a661e2225540591f03257462cb4697a2051792e
Borislav Pecherskikh <borislavtarget@gmail.com>
2023-02-26 16:48:43 +0500

    123

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,21 +1,42 @@
 from rest_framework import serializers
+import datetime as dt
+import webcolors
+from djoser.serializers import UserSerializer
 
-from .models import Cat, Owner, Achievement, AchievementCat
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
+
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+    
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+        
 
 class AchievementsSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
     #owner = serializers.StringRelatedField(read_only=True)
-    achievements = AchievementsSerializer(many=True, required=False)
+    achievements = AchievementsSerializer(many=True)
+    age = serializers.SerializerMethodField()
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
         if 'achievements' not in self.initial_data:
@@ -36,5 +57,14 @@ class OwnerSerializer(serializers.ModelSerializer):
         model = Owner
         fields = ('first_name', 'last_name', 'cats')
 
-#
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+    
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color') 
 
+class CustomUserSErializer(UserSerializer):
+    class Meta:
+        model = UserSerializer
+        fields = ('email', 'id', 'username', 'first_name', 'last_name')
--- cats/views.py
+++ cats/views.py
@@ -1,14 +1,64 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import mixins
+from django.shortcuts import get_object_or_404
+from djoser.views import UserViewSet
+
 
 from .models import Cat, Owner
-from .serializers import CatSerializer, OwnerSerializer
+from .serializers import CatSerializer, OwnerSerializer, CatListSerializer, CustomUserSErializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CatListSerializer
+        return CatSerializer
+    
+    @action(detail=False, url_path='recent-awhite-cats')
+    def recent_white_cats(self, request):
+        cats = Cat.objects.filter(color='White')[:5]
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
+
 
 class OwnerViewSet(viewsets.ModelViewSet):
     queryset = Owner.objects.all()
     serializer_class = OwnerSerializer
     
+
+class UpdateDeleteViewSet(
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+    ):
+    pass
+
+
+class CreateRetrieveViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                            viewsets.GenericViewSet):
+    pass
+
+
+class LightCatViewSet(CreateRetrieveViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer 
+
+class CustomUserViewSet(UserViewSet):
+    pass
+
+# class CatViewSet(viewsets.ViewSet):
+#     def list(self, request):
+#         queryset =Cat.objects.all()
+#         serializer = CatSerializer(queryset, many=True)
+#         return Response(serializer.data)
+    
+#     def retrieve(self, request, pk=None):
+#         queryset=Cat.objects.all()
+#         cat = get_object_or_404(queryset, pk=pk)
+#         serializer = CatSerializer(cat)
+#         return Response(serializer.data)
